6b5e69539d577882c5aba5eb65c13c6b87fad312
Pkylik <65322459+Pkylik@users.noreply.github.com>
2021-07-09 23:04:49 +1000

    Update SetStatus.cs
    Added extra statuses and made editing the array count no longer needed, this also makes the code much cleaner

--- main/Services/SetStatus.cs
+++ main/Services/SetStatus.cs
@@ -22,23 +22,83 @@ namespace TonTon_Discord_Bot.Services
             _commands = commands;
         }
 
+
+
+        //Variables so that I can easily make changes for the arrays (Don't change values here)
+        int watchCount = 0;
+        int playCount = 0;
+        int listenCount = 0;
+
+        //Arrays
+        string[] watchingStatuses =
+        {
+            "How to tell if you're getting too fat",
+            "EASY Fries Recipe (No Oil)",
+            "Gawr Gura Ch. Videos",
+            "Korone Ch. Videos",
+            "Should I be worried if i'm overweight?",
+            "vines",
+            "random Twitter videos",
+            "Doom Crossing - Official Teaser",
+            "cat videos",
+            "parrot videos",
+            "Pfp by ぶくろて: https://www.pixiv.net/en/artworks/78180605"
+        };
+
+        string[] playingStatuses =
+        {
+            "Doom Crossing",
+            "Animal Crossing: New Leaf",
+            "Animal Crossing: New Horizons",
+            "Slime Rancher",
+            "Stardew Valley",
+            "Kerbal Space Program",
+            "Adobe Photoshop 2021",
+            "Nintendogs",
+            "Pfp by ぶくろて: https://www.pixiv.net/en/artworks/78180605"
+        };
+
+        string[] listeningStatuses =
+        {
+            "Yutaka Hirasaka - eau",
+            "A Short Hike (Full OST)",
+            "Broke For Free - Add And",
+            "Broke For Free - Juparo",
+            "Broke For Free - Xxxv",
+            "not Mask by dream because that's a pretty bad song",
+            "Delayde - sunday morning bacon",
+            "Classical music on my radio",
+            "a cool random radio I found",
+            "Animal Crossing: New Leaf OST",
+            "upbeat music",
+            "Pfp by ぶくろて: https://www.pixiv.net/en/artworks/78180605"
+        };
+
         public async Task StartStatus()
         {
+            watchCount = watchingStatuses.Length + 1;
+            playCount = playingStatuses.Length + 1;
+            listenCount = listeningStatuses.Length + 1;
+
             while (true)
             {
-                int startNumb = RandNumbGen(0, 4);
+                int startNumb = RandNumbGen(0, 5);
 
                 if (startNumb == 1)
                 {
-                    await SetWatchingStatus(0, 9);
+                    await SetWatchingStatus();
                 }
                 else if (startNumb == 2)
                 {
-                    await SetPlayingStatus(0, 8);
+                    await SetPlayingStatus();
                 }
                 else if (startNumb == 3)
                 {
-                    await SetListeningStatus(0, 11);
+                    await SetListeningStatus();
+                }
+                else if (startNumb == 4)
+                {
+                    await ClearStatus();
                 }
                 await Task.Delay(1200000); //20 Minutes
             }
@@ -53,65 +113,28 @@ namespace TonTon_Discord_Bot.Services
             return number;
         }
 
-        //Set status and array types
-        //0, 9 (EXCLUSIVE, REMEMBER TO CHECK SECOND VALUE. ALSO REMEMBER THAT ITEM ONE STARTS AT 0)
-        public async Task SetWatchingStatus(int Numb1, int Numb2)
+        //Set statuses
+        public async Task SetWatchingStatus()
         {
-            string[] watchingStatuses =
-            {
-                "How to tell if you're getting too fat",
-                "EASY Fries Recipe (No Oil)",
-                "Gawr Gura Ch. Videos",
-                "Korone Ch. Videos",
-                "Should I be worried if i'm overweight?",
-                "Vines",
-                "Random Twitter videos",
-                "Doom Crossing - Official Teaser",
-                "Pfp by ぶくろて: https://www.pixiv.net/en/artworks/78180605"
-            };
-
-            await _discord.SetGameAsync(watchingStatuses[RandNumbGen(Numb1, Numb2)], null, ActivityType.Watching);
+            await _discord.SetGameAsync(watchingStatuses[RandNumbGen(0, watchCount)], null, ActivityType.Watching);
             return;
         }
 
-        //0, 8 (EXCLUSIVE, REMEMBER TO CHECK SECOND VALUE. ALSO REMEMBER THAT ITEM ONE STARTS AT 0)
-        public async Task SetPlayingStatus(int Numb1, int Numb2)
+        public async Task SetPlayingStatus()
         {
-            string[] playingStatuses =
-            {
-                "Doom Crossing",
-                "Animal Crossing: New Leaf",
-                "Animal Crossing: New Horizons",
-                "Slime Rancher",
-                "Stardew Valley",
-                "Kerbal Space Program",
-                "Adobe Premier 2021",
-                "Pfp by ぶくろて: https://www.pixiv.net/en/artworks/78180605"
-            };
-
-            await _discord.SetGameAsync(playingStatuses[RandNumbGen(Numb1, Numb2)], null, ActivityType.Playing);
+            await _discord.SetGameAsync(playingStatuses[RandNumbGen(0, playCount)], null, ActivityType.Playing);
             return;
         }
 
-        //0, 11 (EXCLUSIVE, REMEMBER TO CHECK SECOND VALUE. ALSO REMEMBER THAT ITEM ONE STARTS AT 0)
-        public async Task SetListeningStatus(int Numb1, int Numb2)
+        public async Task SetListeningStatus()
         {
-            string[] listeningStatuses =
-            {
-                "Yutaka Hirasaka - eau",
-                "A Short Hike (Full OST)",
-                "Broke For Free - Add And",
-                "Broke For Free - Juparo",
-                "Broke For Free - Xxxv",
-                "not Mask by dream because that's a pretty bad song",
-                "Delayde - sunday morning bacon",
-                "Classical music on my radio",
-                "A random radio I found",
-                "Animal Crossing: New Leaf OST",
-                "Pfp by ぶくろて: https://www.pixiv.net/en/artworks/78180605"
-            };
+            await _discord.SetGameAsync(listeningStatuses[RandNumbGen(0, listenCount)], null, ActivityType.Listening);
+            return;
+        }
 
-            await _discord.SetGameAsync(listeningStatuses[RandNumbGen(Numb1, Numb2)], null, ActivityType.Listening);
+        public async Task ClearStatus()
+        {
+            await _discord.SetGameAsync(null, null, ActivityType.Listening);
             return;
         }
     }
